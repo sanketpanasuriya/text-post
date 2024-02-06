f88450d9a769a7c70acfdc50007f70e87a27d2f8
SanketPansuriya <sanket.panasuriya@bacancy.com>
2024-02-06 21:08:16 +0530

    added fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import './jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
